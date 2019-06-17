82fefcbd02c2038da6178f2fdc6a09cabf595fda
YcodeMan <1130578809@qq.com>
2019-06-17 10:03:39 +0800

    修改别名

--- webpack.config.js
+++ webpack.config.js
@@ -40,11 +40,11 @@ module.exports = {
     resolve: {
         alias: {
             '@':path.join(__dirname, 'src'),
-            'store':path.join(__dirname, 'src/store'),
-            'view':path.join(__dirname, 'src/view'),
-            'common':path.join(__dirname, 'src/common'),
-            'components': path.join(__dirname, 'src/components'),
-            'utils': path.join(__dirname, 'src/utils'),
+            '@store':path.join(__dirname, 'src/store'),
+            '@views':path.join(__dirname, 'src/views'),
+            '@common':path.join(__dirname, 'src/common'),
+            '@components': path.join(__dirname, 'src/components'),
+            '@utils': path.join(__dirname, 'src/utils'),
         }
     }
 }
