f0a20073afbfb154230db7b6384bb82cd6060fc1
zhanxj90 <2602816251@qq.com>
2019-07-01 17:52:42 +0800

    add toDetails

--- src/store/actions/subjectList/actionCreator.js
+++ src/store/actions/subjectList/actionCreator.js
@@ -3,7 +3,6 @@ import {getSubjectList} from '@api/api'
 
 export const actionSubjectList = (parm)=> async (dispatch)=>{
     let data = await getSubjectList(parm);
-    console.log(data)
     return dispatch({
         type:subjectListType,
         value:data
